1e68a0b839d91de2f89cfb943070092a1e8cd1e0
LOOPSHAPE <exe.opcode@gmail.com>
2025-08-24 12:23:12 +0000

    setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/env bash
 
-cp ./bin/ai /usr/local/bin/
+cp ./bin/ai /usr/local/bin/ai.sh
+mv /usr/local/bin/ai.sh /usr/local/bin/ai
 chmod +x /usr/local/bin/ai
 
 echo 'AI auto ... installiert!'
